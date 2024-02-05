02a094aa0b62ae733ae9e1185b3b7dc36ab285af
XZACTA <110074271+XZACTA@users.noreply.github.com>
2024-02-05 22:39:54 +0700

    แก้ icon
    แก้ icon

--- testsgit-app/src/App.js
+++ testsgit-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import rasen from './rasengan.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={rasen} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
